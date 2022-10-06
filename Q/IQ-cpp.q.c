8cade4b006c8b0294eb513e643afebef1501baa8
chgv99 <christiangv99@gmail.com>
2022-10-06 20:15:43 +0100

    print for variables added

---
+++ Q/IQ-cpp.q.c
@@ -0,0 +1,41 @@
+#include "Q.h"
+# 1 "<stdin>"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "<stdin>"
+# 1 "Qlib.h" 1
+# 2 "<stdin>" 2
+BEGIN
+L 0:
+STAT(0)
+CODE(0)
+ R6 = R7;
+STAT(1)
+ DAT(0x11ffc,I,0);
+CODE(1)
+ R1 = 0x11ffc;
+ R0 = 6969;
+ I(R1) = R0;
+ R0 = 1;
+ R2 = I(0x11ffc);
+STAT(2)
+ STR(0x11ff8, "%i\n");
+CODE(2)
+ R1 = 0x11ff8;
+ R0 = 1;
+ GT(-12);
+L 1:
+ R7 = R7 - 4;
+ R0 = 100;
+ I(R7) = R0;
+STAT(3)
+ DAT(0x11ff0,I,0);
+CODE(3)
+ R1 = 0x11ff0;
+ R0 = 3;
+ I(R1) = R0;
+GT(-2);
+END
