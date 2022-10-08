ca75ca5cd1807dee163e9cae0732e2c4e74c6394
CarlosBritoPerez <carlosbritoperez99@gmail.com>
2022-10-08 17:49:59 +0100

    operation with variables implemented

--- Q/IQ-cpp.q.c
+++ Q/IQ-cpp.q.c
@@ -14,63 +14,18 @@ STAT(0)
 CODE(0)
  R7 = R7 - 4;
  R6 = R7;
-STAT(1)
- DAT(0x11ffc,I,0);
-
-CODE(1)
- R0 = 3;
- R1 = 7;
- R0=R0+R1;
-
- I(0x11ffc) = R0;
-
-STAT(2)
- STR(0x11fa8,"Resultado: \n");
-CODE(2)
- I(R7) = R0;
- I(R7 - 4) = R1;
- I(R7 - 8) = R2;
- R7 = R7 - 12;
+ R7 = R7 - 4;
  R0 = 1;
- R1 = 0x11fa8;
- R2 = 0;
- GT(-12);
-L 1:
- R0 = I(R7);
- R1 = I(R7 + 4);
- R2 = I(R7 + 8);
-
-STAT(3)
- STR(0x11ff0,"3\n");
-CODE(3)
- I(R7) = R0;
- I(R7 - 4) = R1;
- I(R7 - 8) = R2;
+ I(R6 - 4) = R0;
+ R7 = R7 - 8;
+ R0 = 0;
+ R1 = I(R6 - 4);
+ R0=R0||R1;
+ I(R6 - 8) = R0;
  R7 = R7 - 12;
- R0 = 2;
- R1 = 0x11ff0;
- R2 = 0;
- GT(-12);
-L 2:
- R0 = I(R7);
- R1 = I(R7 + 4);
- R2 = I(R7 + 8);
-
- I(R7) = R0;
- I(R7 - 4) = R1;
- I(R7 - 8) = R2;
- R7 = R7 - 12;
- R0 = 3;
- R2 = I(0x11ffc);
-STAT(4)
- STR(0x11fe8, "%i\n");
-CODE(4)
- R1 = 0x11fe8;
- R0 = 3;
- GT(-12);
-L 3:
- R0 = I(R7);
- R1 = I(R7 + 4);
- R2 = I(R7 + 8);
+ R0 = I(R6 - 8);
+ R1 = I(R6 - 4);
+ R0=R0&&R1;
+ I(R6 - 12) = R0;
 GT(-2);
 END
