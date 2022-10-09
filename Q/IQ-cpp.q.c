7b28c12bc97035a4fa7aae20a328aac5c0bb9e33
CarlosBritoPerez <carlosbritoperez99@gmail.com>
2022-10-10 00:42:16 +0100

    test

--- Q/IQ-cpp.q.c
+++ Q/IQ-cpp.q.c
@@ -21,46 +21,32 @@ CODE(1)
  R0 = 13;
 
  I(0x11ffc) = R0;
-STAT(2)
- DAT(0x11ff8,I,0);
 
-CODE(2)
- R0 = 8;
-
- I(0x11ff8) = R0;
-STAT(3)
- DAT(0x11ff4,I,0);
-
-CODE(3)
- R0 = 'd';
-
- I(0x11ff4) = R0;
- R0 = 'd';
- R1 = I(0x11ff4);
- R0=R0==R1;
-
- R0 = R0 == 1;
- IF (!R0) GT(1);
+ GT(2);
+L 1:
 
  I(R7 - 4) = R0;
  I(R7 - 8) = R1;
  I(R7 - 12) = R2;
  R7 = R7 - 16;
- R0 = 2;
- R2 = I(0x11ff4);
-STAT(4)
- STR(0x11ff0, "%c\n");
-CODE(4)
- R1 = 0x11ff0;
- R0 = 2;
+ R0 = 3;
+ R2 = I(0x11ffc);
+STAT(2)
+ STR(0x11ff4, "%i\n");
+CODE(2)
+ R1 = 0x11ff4;
+ R0 = 3;
  GT(-12);
-L 2:
+L 3:
  R0 = I(R7 + 12);
  R1 = I(R7 + 8);
  R2 = I(R7 + 4);
  R7 = R7 + 16;
+ I(0x11fec) = 4;
+ GT(4);
+L 2:
 
-
-L 1:
+ R0 = I(0x11fec);
+L R0:
 GT(-2);
 END
